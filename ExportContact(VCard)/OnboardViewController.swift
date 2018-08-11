d014cfd42b05626cd4d496518a4a37144d8c6d88
Mustafa GUNES <gunes149@gmail.com>
2018-08-11 16:53:21 +0300

    Create onboard functions

--- ExportContact(VCard)/OnboardViewController.swift
+++ ExportContact(VCard)/OnboardViewController.swift
@@ -28,3 +28,37 @@ class OnboardViewController: UIViewController {
         userData.synchronize()
     }
 }
+
+extension OnboardViewController: PaperOnboardingDelegate, PaperOnboardingDataSource {
+    func onboardingItemsCount() -> Int {
+        return 3
+    }
+    
+    func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
+        let bgOne = #colorLiteral(red: 0.9734469056, green: 0.4355008006, blue: 0.260989815, alpha: 1)
+        let bgTwo = #colorLiteral(red: 0.9997629523, green: 0.7760336995, blue: 0.1678207517, alpha: 1)
+        let bgThree = #colorLiteral(red: 0.3685051203, green: 0.7292535901, blue: 0.4907024503, alpha: 1)
+        let textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        return [
+        ][index]
+    }
+    
+    func onboardingDidTransitonToIndex(_ index: Int) {
+        if index == 2 {
+            skipButtonOutlet.isHidden = false
+        }
+    }
+    
+    func onboardingWillTransitonToIndex(_ index: Int) {
+        if index != 2 {
+            if skipButtonOutlet.isHidden == false {
+                skipButtonOutlet.isHidden = true
+            }
+        }
+    }
+    
+    func onboardingConfigurationItem(_ item: OnboardingContentViewItem, index: Int) {
+        // TODO : ...
+    }
+}
