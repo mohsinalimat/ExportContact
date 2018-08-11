dbc3bc8303ef393900d52ae1de81d56c924cfc3d
Mustafa GUNES <gunes149@gmail.com>
2018-08-11 19:22:16 +0300

    Custom button created and set constraints in code

--- ExportContact(VCard)/HomeScreen.swift
+++ ExportContact(VCard)/HomeScreen.swift
@@ -7,11 +7,78 @@
 //
 
 import UIKit
+import SwiftyButton
 
 class HomeScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        buttonCreate_SetupLayout()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func buttonCreate_SetupLayout() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            let pressButton = PressableButton()
+            pressButton.shadowHeight = 5
+            //pressButton.cornerRadius = 5
+            pressButton.setTitle("Kişileri Getir", for: .normal)
+            pressButton.titleLabel?.font = UIFont(name: "AvenirNext-DemiBold", size: 50)
+            //pressButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 50) // Font boyutu
+            pressButton.frame = CGRect(x: 153, y: 318, width: 200, height: 100)
+            pressButton.colors = .init(button: UIColor(red:0.87, green:0.29, blue:0.25, alpha:1.0), shadow: UIColor(red:0.75, green:0.22, blue:0.17, alpha:1.0))
+            
+            let pressButton2 = PressableButton()
+            pressButton2.shadowHeight = 5
+            pressButton2.setTitle("Yedekle", for: .normal)
+            pressButton2.frame = CGRect(x: 153, y: 318, width: 200, height: 100)
+            pressButton2.titleLabel?.font = UIFont(name: "AvenirNext-DemiBold", size: 50)
+            pressButton2.colors = .init(button: UIColor(red:0.29, green:0.62, blue:0.26, alpha:1.0), shadow: UIColor(red:0.25, green:0.53, blue:0.22, alpha:1.0))
+            
+            pressButton2.addTarget(nil, action: #selector(self.tapped), for: .touchUpInside) // UIAlert çalıştırarak seçilen satora göre işlem yapacak.
+            
+            self.view.addSubview(pressButton)
+            self.view.addSubview(pressButton2)
+            
+            pressButton.translatesAutoresizingMaskIntoConstraints = false
+            pressButton.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+            pressButton.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+            pressButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+            pressButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            pressButton.heightAnchor.constraint(equalToConstant: 368).isActive = true
+            
+            
+            pressButton2.translatesAutoresizingMaskIntoConstraints = false
+            pressButton2.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+            pressButton2.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+            pressButton2.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+            pressButton2.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            pressButton2.heightAnchor.constraint(equalToConstant: 368).isActive = true
+        }
+    }
+    
+    @objc func tapped() {
+        multiAlert()
+    }
+    
+    func multiAlert() {
+        let alertController = UIAlertController(title: "Deneme", message: "alertMessage", preferredStyle: .actionSheet)
+        
+        let alert = UIAlertAction(title: "VCARD", style: .default) { (alert) in
+            print("alert1 çalışıyor")
+        }
+        let alert2 = UIAlertAction(title: "CSV", style: .default) { (alert2) in
+            print("alert2 çalışıyor")
+        }
+        let alertCancel = UIAlertAction(title: "CLOSE", style: .destructive, handler: nil)
+        
+        alertController.addAction(alert)
+        alertController.addAction(alert2)
+        alertController.addAction(alertCancel)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
-
