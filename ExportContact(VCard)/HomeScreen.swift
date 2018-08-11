f3ea5d6a25fa2283abf216af72ac0415ae4835a9
Mustafa GUNES <gunes149@gmail.com>
2018-08-11 20:14:09 +0300

    Model imported HomeScreen

--- ExportContact(VCard)/HomeScreen.swift
+++ ExportContact(VCard)/HomeScreen.swift
@@ -11,14 +11,24 @@ import SwiftyButton
 
 class HomeScreen: UIViewController {
 
+    private var model = ContactModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // TODO : Kişileri getir denildiğinde alert ile ekrana kaç kişi olduğunu göstert.
+        
+        model.exportingContactCallback = exportToCSV_VCARDCallback
         buttonCreate_SetupLayout()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    func exportToCSV_VCARDCallback(path: URL){
+        let vc = UIActivityViewController(activityItems: [path], applicationActivities: [])
+        present(vc, animated: true, completion: nil)
     }
     
     func buttonCreate_SetupLayout() {
@@ -32,6 +42,8 @@ class HomeScreen: UIViewController {
             pressButton.frame = CGRect(x: 153, y: 318, width: 200, height: 100)
             pressButton.colors = .init(button: UIColor(red:0.87, green:0.29, blue:0.25, alpha:1.0), shadow: UIColor(red:0.75, green:0.22, blue:0.17, alpha:1.0))
             
+            pressButton.addTarget(nil, action: #selector(self.pressButtonPressed), for: .touchUpInside)
+            
             let pressButton2 = PressableButton()
             pressButton2.shadowHeight = 5
             pressButton2.setTitle("Yedekle", for: .normal)
@@ -39,7 +51,7 @@ class HomeScreen: UIViewController {
             pressButton2.titleLabel?.font = UIFont(name: "AvenirNext-DemiBold", size: 50)
             pressButton2.colors = .init(button: UIColor(red:0.29, green:0.62, blue:0.26, alpha:1.0), shadow: UIColor(red:0.25, green:0.53, blue:0.22, alpha:1.0))
             
-            pressButton2.addTarget(nil, action: #selector(self.tapped), for: .touchUpInside) // UIAlert çalıştırarak seçilen satora göre işlem yapacak.
+            pressButton2.addTarget(nil, action: #selector(self.pressButton2Pressed), for: .touchUpInside) // UIAlert çalıştırarak seçilen satora göre işlem yapacak.
             
             self.view.addSubview(pressButton)
             self.view.addSubview(pressButton2)
@@ -61,7 +73,11 @@ class HomeScreen: UIViewController {
         }
     }
     
-    @objc func tapped() {
+    @objc func pressButtonPressed() {
+        // TODO : alertaction'da neye basarsa onun uzantısını alacak.
+    }
+    
+    @objc func pressButton2Pressed() {
         multiAlert()
     }
     
